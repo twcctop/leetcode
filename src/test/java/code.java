a3c1e56326e64ee84c9e65bb3a1852b1819e47ac
田文超 <30425369+chaochao214@users.noreply.github.com>
2021-03-03 16:17:35 +0800

    0303

--- src/test/java/code.java
+++ src/test/java/code.java
@@ -831,6 +831,36 @@ public class code {
         }
          return  ans;
     }
+     @Test
+    public void test198(){
+          int[] arr= {2,7,9,3,1};
+         System.out.println(rob(arr));
+
+     }
+
+        public int rob(int[] nums) {
+            int len = nums.length;
+            if(len==0){
+                return   0;
+            }
+            if(len==1){
+                return nums[0];
+            }
+            if(len==2){
+                return Math.max(nums[0],nums[1]);
+            }
+            int first= nums[0];
+            int second  = Math.max(first,nums[1]);
+            int res=0;
+            for(int i=2;i<len;i++){
+                int temp =second;
+                res= Math.max(second,nums[i]+first);
+                second=res;
+                first=temp;
+            }
+            return res;
+        }
+
 }
 
 
