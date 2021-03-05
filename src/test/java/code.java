9e07553bfac7ac66f418d179b26862049c66191c
田文超 <30425369+chaochao214@users.noreply.github.com>
2021-03-05 11:29:24 +0800

    dp动态规划

--- src/test/java/code.java
+++ src/test/java/code.java
@@ -838,28 +838,105 @@ public class code {
 
      }
 
-        public int rob(int[] nums) {
-            int len = nums.length;
-            if(len==0){
-                return   0;
-            }
-            if(len==1){
-                return nums[0];
-            }
-            if(len==2){
-                return Math.max(nums[0],nums[1]);
-            }
-            int first= nums[0];
-            int second  = Math.max(first,nums[1]);
-            int res=0;
-            for(int i=2;i<len;i++){
-                int temp =second;
-                res= Math.max(second,nums[i]+first);
-                second=res;
-                first=temp;
-            }
-            return res;
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return 0;
         }
+        if (len == 1) {
+            return nums[0];
+        }
+        if (len == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int first = nums[0];
+        int second = Math.max(first, nums[1]);
+        int res = 0;
+        for (int i = 2; i < len; i++) {
+            int temp = second;
+            res = Math.max(second, nums[i] + first);
+            second = res;
+            first = temp;
+        }
+        return res;
+    }
+    @Test
+    public void  test121(){
+        int[] arr= {7,1,5,3,6,4};
+        System.out.println(maxProfit(arr));
+    }
+
+    public int maxProfit(int[] prices) {
+        if (prices.length==0) {
+            return 0;
+        }
+        if (prices.length==1) {
+            return 1;
+        }
+        // int[i][是否持股]
+        int[][] dp = new int[prices.length][2];
+        dp[0][0]= 0;
+        dp[0][1]= -prices[1];
+        for (int i = 1; i < prices.length; i++) {
+            // 不持股
+            dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1]);
+            // 持股
+            dp[i][1] = dp[i-1][0]+prices[i];
+
+        }
+       return 0;
+    }
+    @Test
+    public  void test208(){
+        Trie trie = new Trie();
+        trie.insert("apple");
+        trie.search("apple");   // 返回 true
+        trie.search("app");     // 返回 false
+        trie.startsWith("app"); // 返回 true
+        trie.insert("app");
+        trie.search("app");     // 返回 true
+    }
+
+    class Trie {
+         private  boolean is_string =false;
+         private  Trie next[] =new Trie[26];
+
+
+        //R links to node children
+
+
+
+        /** Initialize your data structure here. */
+        public Trie() {
+
+        }
+
+        /** Inserts a word into the trie. */
+        public void insert(String word) {
+            Trie root = this;
+            char w[] = word.toCharArray();
+            for (int i = 0; i < w.length; i++) {
+//                  if(root.next[w[i]-'a']==null)
+                if (root.next[w[i]-'a'] == null) {
+                    root.next[w[i]-'a'] = new Trie();
+                }
+                    root = root.next[w[i]-'a'];
+
+            }
+
+        }
+
+        /** Returns if the word is in the trie. */
+        public boolean search(String word) {
+          return  false;
+        }
+
+        /** Returns if there is any word in the trie that starts with the given prefix. */
+        public boolean startsWith(String prefix) {
+            return  false;
+        }
+    }
+
 
 }
 
