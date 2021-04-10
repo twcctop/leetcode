714d7afb4934d47b55d8c53f0824e1286e7abef3
dazuiTian <30425369+chaochao214@users.noreply.github.com>
2021-04-10 23:55:38 +0800

    排序算法

--- src/test/java/算法训练营覃超题目/_18排序算法.java
+++ src/test/java/算法训练营覃超题目/_18排序算法.java
@@ -1,6 +1,14 @@
 package 算法训练营覃超题目;
 
 import org.junit.Test;
+import org.omg.CORBA.ARG_IN;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Author: twc
@@ -14,14 +22,45 @@ public class _18排序算法 {
      **/
     @Test
     public void test1122() {
+//        int[]  arr1 = {2,3,1,3,2,4,6,7,9,2,19};
+//        int[]  arr2= {2,1,4,3,9,6};
+//        System.out.println(Arrays.stream(relativeSortArray(arr1, arr2)).boxed().collect(Collectors.toList()));
 
+        int[]  arr1 = {2,3,1,3,2,4,6,7,9,2,19};
+        int[]  arr2= {2,1,4,3,9,6};
+        System.out.println(Arrays.stream(relativeSortArray(arr1, arr2)).boxed().collect(Collectors.toList()));
     }
 
-    public int[] relativeSortArray(int[] arr1, int[] arr2) {
-
-
-
-
-        return  null;
+    public int[] relativeSortArray(int[] arr1, int[] arr2)  {
+         int[]  res  =  new int[arr1.length];
+         List<Integer> arr2List = Arrays.stream(arr2).boxed().collect(Collectors.toList());
+        List<Integer> arr2ListNotRemove = Arrays.stream(arr2).boxed().collect(Collectors.toList());
+        List<Integer>  notCollect =  new ArrayList<>();
+         int flag = 0;
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr2List.contains(arr1[i])) {
+                //
+                for (int j : arr1) {
+                    if (j==arr1[i]) {
+                        res[flag]= arr1[i];
+                        flag++;
+                    }
+                }
+                for (int k = 0; k < arr2List.size(); k++) {
+                    if (arr2List.get(k)== arr1[i]) {
+                         arr2List.remove(k);
+                    }
+                }
+            }else{
+                if (!arr2ListNotRemove.contains(arr1[i])) {
+                notCollect.add(arr1[i]);
+                }
+            }
+        }
+        for (Integer integer : notCollect) {
+              res[flag] = integer;
+              flag++;
+        }
+        return  res;
     }
 }
---
+++ src/test/java/自建测试/testConvertArrToList.java
@@ -0,0 +1,22 @@
+package 自建测试;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @Author: twc
+ * @Date 2021/4/10 23:29
+ **/
+public class testConvertArrToList {
+    @Test
+    public void testArrays(){
+        int[] spam = new int[] { 1, 2, 3 };
+        List<Integer> res = Arrays.stream(spam).boxed().collect(Collectors.toList());
+        System.out.println(res);
+    }
+
+
+}
