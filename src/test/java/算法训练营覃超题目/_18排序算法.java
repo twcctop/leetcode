cc707536aa1b760ed404fca9ff3006e63166ab30
dazuiTian <30425369+chaochao214@users.noreply.github.com>
2021-04-17 23:41:04 +0800

    排序算法：二维数组

--- src/test/java/算法训练营覃超题目/_18排序算法.java
+++ src/test/java/算法训练营覃超题目/_18排序算法.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.omg.CORBA.ARG_IN;
 
 import java.awt.image.AreaAveragingScaleFilter;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -26,41 +27,109 @@ public class _18排序算法 {
 //        int[]  arr2= {2,1,4,3,9,6};
 //        System.out.println(Arrays.stream(relativeSortArray(arr1, arr2)).boxed().collect(Collectors.toList()));
 
-        int[]  arr1 = {2,3,1,3,2,4,6,7,9,2,19};
-        int[]  arr2= {2,1,4,3,9,6};
+        int[] arr1 = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
+        int[] arr2 = {2, 1, 4, 3, 9, 6};
         System.out.println(Arrays.stream(relativeSortArray(arr1, arr2)).boxed().collect(Collectors.toList()));
     }
 
-    public int[] relativeSortArray(int[] arr1, int[] arr2)  {
-         int[]  res  =  new int[arr1.length];
-         List<Integer> arr2List = Arrays.stream(arr2).boxed().collect(Collectors.toList());
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] res = new int[arr1.length];
+        List<Integer> arr2List = Arrays.stream(arr2).boxed().collect(Collectors.toList());
         List<Integer> arr2ListNotRemove = Arrays.stream(arr2).boxed().collect(Collectors.toList());
-        List<Integer>  notCollect =  new ArrayList<>();
-         int flag = 0;
+        List<Integer> notCollect = new ArrayList<>();
+        int flag = 0;
         for (int i = 0; i < arr1.length; i++) {
             if (arr2List.contains(arr1[i])) {
                 //
                 for (int j : arr1) {
-                    if (j==arr1[i]) {
-                        res[flag]= arr1[i];
+                    if (j == arr1[i]) {
+                        res[flag] = arr1[i];
                         flag++;
                     }
                 }
                 for (int k = 0; k < arr2List.size(); k++) {
-                    if (arr2List.get(k)== arr1[i]) {
-                         arr2List.remove(k);
+                    if (arr2List.get(k) == arr1[i]) {
+                        arr2List.remove(k);
                     }
                 }
-            }else{
+            } else {
                 if (!arr2ListNotRemove.contains(arr1[i])) {
-                notCollect.add(arr1[i]);
+                    notCollect.add(arr1[i]);
                 }
             }
         }
         for (Integer integer : notCollect) {
-              res[flag] = integer;
-              flag++;
+            res[flag] = integer;
+            flag++;
         }
-        return  res;
+        return res;
     }
+
+    /**
+     * @Description
+     * @Date 2021/4/17 22:30
+     **/
+    @Test
+    public void test242() {
+        System.out.println(isAnagram("anagram", "nagaram"));
+    }
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        if (s.length() == 0) {
+            return true;
+        }
+        char[] charsS = s.toCharArray();
+        char[] charsT = t.toCharArray();
+        Arrays.sort(charsS);
+        Arrays.sort(charsT);
+        return new String(charsS).equals(new String(charsT));
+    }
+
+    /**
+     * @Description
+     * @Date 2021/4/17 22:48
+     **/
+    @Test
+    public void test56() {
+
+    }
+
+    public int[][] merge(int[][] intervals) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            for (int j = 0; j < intervals[i].length; j++) {
+                if (!list.contains(intervals[i][j])) {
+                    list.add(intervals[i][j]);
+                }
+            }
+        }
+        Collections.sort(list);
+        int start = 0;
+        int end = 0;
+
+        List<int[]> res = new ArrayList<>();
+        //list 间断的转为 arr  123 567 -> [1,2】 【57】
+        for (int i = 0; i < list.size(); i++) {
+            if (i > 0) {
+                if (list.get(i) != list.get(i) - 1) {
+                    end = i - 1;
+                    int[] element = new int[2];
+                    element[0] = start;
+                    element[1] = end;
+                    start = i;
+                    res.add(element);
+                }
+            }
+
+        }
+        int[][]   arrRes =  new int[res.size()][2];
+        for (int i = 0; i < arrRes.length; i++) {
+            arrRes[i] = arrRes[i];
+        }
+        return arrRes ;
+    }
+
 }
--- src/test/java/自建测试/testTwoDimensionalArray.java
+++ src/test/java/自建测试/testTwoDimensionalArray.java
@@ -2,15 +2,59 @@ package 自建测试;
 
 import org.junit.Test;
 
+import java.util.Objects;
+
 /**
  * @Author: twc
  * @Date 2021/3/17 18:25
  **/
 public class testTwoDimensionalArray {
     @Test
-    public  void testChar(){
-        char[][] arr= new char[2][3];
+    public void testChar() {
+        char[][] arr = new char[][]{{1,2,3},{4,5,6},{7,8,9}};
+        // 行 列 数
         System.out.println(arr.length);
-    }
+        System.out.println(arr[0].length);
+//        System.out.println(new String(String.valueOf(arr)));
+        // 遍历元素
+/*        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.println(arr[i][j]+" ");
+//                if (Objects.isNull(arr[i][j])||arr[i][j]==0) {
+//                    System.out.print("test");
+//                } else {
+//                    System.out.print(arr[i][j]);
+//                }
+            }*/
 
+
+            for(int a=0;a<arr.length;a++){//控制每个一维数组
+                for(int i=0;i<arr[a].length;i++){//控制每个一维数组中的元素
+                    // todo  char中值存的是int类型 导致失效
+                    char c = arr[a][i];
+                    String s= String.valueOf(c);
+                    System.out.println(s +" ");
+                }
+                System.out.println();//每执行完一个一维数组换行
+            }
+            System.out.println();
+        }
+
+    @Test
+    public void testTwoDecimal(){
+        int[][] arr = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        // 遍历第一个一维数组
+        for(int i=0;i<arr[0].length;i++){
+            System.out.print(arr[0][i]+" ");
+        }
+        System.out.println();
+
+        System.out.println("遍历整个数组");
+        for(int a=0;a<arr.length;a++){//控制每个一维数组
+            for(int i=0;i<arr[a].length;i++){//控制每个一维数组中的元素
+                System.out.print(arr[a][i]+" ");//输出每个元素的值
+            }
+            System.out.println();//每执行完一个一维数组换行
+        }
+    }
 }
