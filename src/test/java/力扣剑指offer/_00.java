364469622d076f9cde020c0f65f270462a6ae3d0
田文超 <30425369+chaochao214@users.noreply.github.com>
2021-05-06 10:27:02 +0800

    剑指offer

--- src/test/java/力扣剑指offer/_00.java
+++ src/test/java/力扣剑指offer/_00.java
@@ -1,10 +1,12 @@
 package 力扣剑指offer;
 
+import entity.ListNode;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @Author: twc
@@ -30,5 +32,43 @@ public class _00 {
              list.add(num);
         }
         return  -1;
+    }
+
+    //04
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix.length==0 || matrix[0].length==0) {
+             return false;
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][0]> target || matrix[i][matrix[0].length-1]<target) {
+                 continue;
+            }
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] ==  target) {
+                     return  true;
+                }
+            }
+        }
+        return  false;
+    }
+
+    //05
+    public String replaceSpace(String s) {
+        return s.replaceAll(" ", "%20");
+    }
+
+    //06
+    public int[] reversePrint(ListNode head) {
+        List<Integer> list=new ArrayList<>();
+        while (Objects.nonNull(head)){
+               list.add(head.val);
+               head = head.next;
+        }
+        int[] res =new int[list.size()];
+        for (int i = list.size()-1; i>=0; i--) {
+             res[i]= list.get(i);
+        }
+        return res;
     }
 }
