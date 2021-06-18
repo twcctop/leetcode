e395409db5f9f36686839c35b41cf2a7a00e3f44
田文超 <30425369+chaochao214@users.noreply.github.com>
2021-06-18 10:35:17 +0800

    数学部分

--- src/test/java/leetbook/图解算法数据结构/_09数学.java
+++ src/test/java/leetbook/图解算法数据结构/_09数学.java
@@ -21,4 +21,15 @@ public class _09数学 {
     }
 
 
+    //57-2  和为s的连续，正数序列
+//    public int[][] findContinuousSequence(int target) {
+//         int i=0,j=target;
+//
+//    }
+//    public  int sum(int i,int j){
+//        (j-i+1)
+//         return  (i+j)*(j-i+1)/2;
+//    }
+
+
 }
--- src/test/java/每日一题/_2021年6月.java
+++ src/test/java/每日一题/_2021年6月.java
@@ -12,4 +12,10 @@ public class _2021年6月 {
         //
         return  true;
     }
+
+    //最小好进制
+    public String smallestGoodBase(String n) {
+
+        return  "";
+    }
 }
