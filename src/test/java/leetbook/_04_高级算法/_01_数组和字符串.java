040851bbdd97bf21a0a7b72e1c8aed7bf03d75e9
chaochao214 <505530098@qq.com>
2021-08-24 09:16:17 +0800

    co

--- src/test/java/leetbook/_04_高级算法/_01_数组和字符串.java
+++ src/test/java/leetbook/_04_高级算法/_01_数组和字符串.java
@@ -4,13 +4,29 @@ public class _01_数组和字符串 {
 
     // 除自身以外数组的乘积
     public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] left =new int[length];
+        int[] right= new int[length];
+        int[] res= new int[length];
 
-        return null;
+        left[0]=1;
+        for (int i = 1; i < left.length; i++) {
+             left[i]= nums[i-1]*left[i-1];
+        }
+
+        right[length-1]=1;
+        for (int i = length-2; i>=0; i--) {
+             right[i]= nums[i+1]* right[i+1];
+        }
+
+
+        for (int i = 0; i < length; i++) {
+            res[i]=left[i]*right[i];
+        }
+
+        return res;
 
     }
 
-    //测试提交
-    //测试提交记录2
-    //测试提交记录3
-    //测试提交记录4
+
 }
