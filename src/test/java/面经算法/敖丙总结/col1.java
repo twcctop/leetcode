cc472b4891476219a06606b7a2bae24809b77167
dazuiTian <505530098@qq.com>
2021-08-31 22:29:25 +0800

    commit

--- src/test/java/面经算法/敖丙总结/col1.java
+++ src/test/java/面经算法/敖丙总结/col1.java
@@ -6,7 +6,18 @@ import java.util.List;
 
 public class col1 {
 
+    // 二叉树右侧视图
     public List<Integer> rightSideView(TreeNode root) {
         return null;
     }
+
+
+    // 四数之和
+        // 排序加双指针
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+
+        return  null;
+    }
+
+
 }
