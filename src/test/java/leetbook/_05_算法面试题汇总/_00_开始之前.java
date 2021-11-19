5e36295b35532094001f4215d58eb918a0b9e06b
chaochao214 <505530098@qq.com>
2021-11-19 17:59:28 +0800

    测试提交

---
+++ src/test/java/leetbook/_05_算法面试题汇总/_00_开始之前.java
@@ -0,0 +1,18 @@
+package leetbook._05_算法面试题汇总;
+
+public class _00_开始之前 {
+
+    //只出现一次的数字
+    public int singleNumber(int[] nums) {
+
+      return  0;
+    }
+
+    // 鸡蛋掉落  hard
+    public int superEggDrop(int k, int n) {
+
+
+
+    return 0;
+    }
+}
---
+++ src/test/java/leetbook/_05_算法面试题汇总/_01_字符串.java
@@ -0,0 +1,13 @@
+package leetbook._05_算法面试题汇总;
+
+import java.util.List;
+
+public class _01_字符串 {
+
+    public List<List<String>> partition(String s) {
+
+    }
+
+    public String longestPalindrome(String s) {
+    }
+}
