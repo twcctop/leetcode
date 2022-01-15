807b7a5d9d0dd8fe649270847e3f36512a1e2ffb
田文超 <505530098@qq.com>
2022-01-15 19:18:11 +0800

    测试提交

--- src/test/java/leetbook/_05_算法面试题汇总/_01_字符串.java
+++ src/test/java/leetbook/_05_算法面试题汇总/_01_字符串.java
@@ -1,13 +1,15 @@
 package leetbook._05_算法面试题汇总;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class _01_字符串 {
 
     public List<List<String>> partition(String s) {
-
+     return  new ArrayList<>();
     }
 
     public String longestPalindrome(String s) {
+        return "0";
     }
 }
