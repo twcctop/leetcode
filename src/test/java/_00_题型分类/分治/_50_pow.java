5a3de795c5ecc5e745f9e6164e030e39891a5fc8
田文超 <505530098@qq.com>
2022-01-28 23:42:13 +0800

    分治

---
+++ src/test/java/_00_题型分类/分治/_50_pow.java
@@ -0,0 +1,19 @@
+package _00_题型分类.分治;
+
+import org.junit.Test;
+
+public class _50_pow {
+
+  /**
+   * @Description
+   * @Date 2022/1/28 23:39
+   **/
+  @Test
+  public void test() {
+  }
+  public double myPow(double x, int n) {
+
+     return
+
+  }
+}
