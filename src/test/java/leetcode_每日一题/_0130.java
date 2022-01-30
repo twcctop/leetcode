23738ceee55ac4a8300ebbb9113edf7271166508
田文超 <505530098@qq.com>
2022-01-30 11:44:56 +0800

    278

---
+++ src/test/java/leetcode_每日一题/_0130.java
@@ -0,0 +1,24 @@
+package leetcode_每日一题;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class _0130 {
+
+  /**
+   * @Description
+   * @Date 2022/1/30 11:37
+   **/
+  @Test
+  public void test() {
+  }
+
+  public String[] uncommonFromSentences(String s1, String s2) {
+
+    String[] s = s1.split(" ");
+    String[] s2a = s2.split(" ");
+    return new String[];
+  }
+}
