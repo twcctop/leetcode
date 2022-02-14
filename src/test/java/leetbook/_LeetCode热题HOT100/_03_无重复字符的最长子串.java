bb81bef105789dae3323bcf470802df415b304e4
田文超 <505530098@qq.com>
2022-02-14 10:10:29 +0800

    1

---
+++ src/test/java/leetbook/_LeetCode热题HOT100/_03_无重复字符的最长子串.java
@@ -0,0 +1,25 @@
+package leetbook._LeetCode热题HOT100;
+
+public class _03_无重复字符的最长子串 {
+
+
+
+  public int lengthOfLongestSubstring(String s) {
+    char[] chars = s.toCharArray();
+
+    var i =0;
+    var j =0;
+
+
+
+    if(i<chars.length){
+
+        j++;
+
+    }
+
+    return 0;
+  }
+
+
+}
---
+++ src/test/java/leetbook/_LeetCode热题HOT100/readme.markdown
@@ -0,0 +1,1 @@
+https://leetcode-cn.com/problem-list/2cktkvj/
